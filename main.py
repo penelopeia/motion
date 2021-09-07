63009ec80cd41454b15a792aa805f8f299408b79
Kelsi B <find.kelsi@gmail.com>
2021-09-06 21:25:48 -0700

    hide functions not in use

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ while True: #cap.isOpened():
         print("Frame not recieved. Exiting ...")
         break
 
-    frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    filter_frame = filter_white(frame_HSV)
+    #frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #filter_frame = filter_white(frame_HSV)
 
     # mask it on original
-    result = cv2.bitwise_and(frame, frame, mask=filter_frame)
+    #result = cv2.bitwise_and(frame, frame, mask=filter_frame)
 
     grad = corner_gradient(frame)
 
