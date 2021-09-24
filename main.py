597cd8c0e067b36a4c76e1764a0eb192720c4482
Kelsi B <find.kelsi@gmail.com>
2021-09-24 09:20:59 -0700

    moving away from camera dependancies

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 import cv2
+import numpy as np
 
 from color import filter_green, filter_white
 from draw import corner_gradient, fill_gradient, checkerboard_gradient
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-while True: #cap.isOpened():
+if cap.isOpened():
+    while True: #cap.isOpened():
 
-    ret, frame = cap.read()
+        ret, frame = cap.read()
 
-    if not ret:
-        print("Frame not recieved. Exiting ...")
-        break
+        if not ret:
+            print("Frame not recieved. Exiting ...")
+            break
 
-    #frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    #filter_frame = filter_white(frame_HSV)
+        #frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        #filter_frame = filter_white(frame_HSV)
 
-    # mask it on original
-    #result = cv2.bitwise_and(frame, frame, mask=filter_frame)
+        # mask it on original
+        #result = cv2.bitwise_and(frame, frame, mask=filter_frame)
 
-    grad = checkerboard_gradient(frame)
+        grad = checkerboard_gradient(frame)
 
-    cv2.imshow('frame', grad)
-    if cv2.waitKey(1) == ord('q'):
-        break
+        cv2.imshow('frame', grad)
+        if cv2.waitKey(1) == ord('q'):
+            print(frame.shape)
+            print(type(frame))
+            break
+else:
+    frame = np.zeros((480, 640, 3))
+    while True:
+        grad = checkerboard_gradient(frame)
+
+        cv2.imshow('frame', grad)
+        if cv2.waitKey(1) == ord('q'):
+            print(frame.shape)
+            print(type(frame))
+            break
+    pass
 
 cap.release()
 cv2.destroyAllWindows()
